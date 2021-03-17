88c8bfb02dffd92ca3423f8ffe884d28eaeab6e2
Zaid Mohammed Qamar <80026874+qamarzaid@users.noreply.github.com>
2021-03-17 22:20:42 +0530

    quadratic probing code uploaded

---
+++ hashing_quad.py
@@ -0,0 +1,24 @@
+size=int(input("enter size"))
+h_ash=["_"]* size
+
+for i in range(0,size):
+    ele=int(input("enter entry ; "))
+    if ele>0:
+        M = ele % size
+        if h_ash[M]=="_":
+		h_ash[M]=ele
+	else:
+		N=M
+		i=1
+		while h_ash[N]!="_":
+			N=(M+i*i)%size
+			i=i+1
+		h_ash[N]=ele
+    else:
+        pass
+print(h_ash)
+print("-----HASH TABLE-----")
+print("|   INDEX | Entry   | ")
+for i in range (0,size):
+     print("|",i,"|" ,h_ash[i], "| ")
+     print("--------------------")
